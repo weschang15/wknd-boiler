2a452bd74d81892f507e551883f5ac88bbff6ee2
Wesley Chang <weschang15@gmail.com>
2024-06-29 13:26:47 -0700

    feat: api component setup

---
+++ components/api.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const healthRoutes = require("./health/api/routes");
+
+const api = express();
+// global config
+api.set("port", process.env.PORT || 6173);
+
+// API Route resource paths
+api.use("/health", healthRoutes.router);
+
+module.exports = api;
